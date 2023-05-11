996ae2e265a60735b3a3600a4dfa589476676d92
Juan Alfonsus Napitupulu <juanalfonsusn@gmail.com>
2023-05-11 15:25:33 +0700

    mengisi html dan form dikit

--- index.js
+++ index.js
@@ -36,52 +36,82 @@ const daftarProduk = [
     { id: 10, namaProduk: "Jaket Jeje Citayem", penjual: "CFW", tipe: "Baju", gender: 2, stock: 45, img: "produk/jejecitayem.jpg", harga: 82000 },
     { id: 11, namaProduk: "Baju Pantai", penjual: "Raja Fashion", tipe: "Celana", gender: 0, stock: 23, img: "produk/bajupantai.jpg", harga: 30000 },
     { id: 12, namaProduk: "Celana Jeans Mamen", penjual: "Lepis Indonesia", tipe: "Celana", gender: 1, stock: 45, img: "produk/celanajeansmamen.jpg", harga: 38000 },
-    { id: 13, namaProduk: "Celana Jeans Sis", penjual: "Lepis Indonesia", tipe: "Baju", gender: 2, stock: 32, img: "produk/celanajeansis.jpg", harga: 85000 },
+    { id: 13, namaProduk: "Celana Jeans Sis", penjual: "Lepis Indonesia", tipe: "Celana", gender: 2, stock: 32, img: "produk/celanajeansis.jpg", harga: 85000 },
     { id: 14, namaProduk: "Kemeja Sis", penjual: "Raja Fashion", tipe: "Baju", gender: 2, stock: 36, img: "produk/kemejasis.jpg", harga: 82000 },
     { id: 15, namaProduk: "Daster Bunda", penjual: "Rimiyini", tipe: "Celana", gender: 2, stock: 39, img: "produk/DasterBunda.jpg", harga: 30000 },
     { id: 16, namaProduk: "Piyama Bakekok", penjual: "Rimiyini", tipe: "Celana", gender: 2, stock: 12, img: "produk/piyama.jpg", harga: 38000 },
     { id: 17, namaProduk: "Baju Adimas", penjual: "Galaksi Sport", tipe: "Baju", gender: 0, stock: 45, img: "produk/adimas.jpg", harga: 85000 },
-    { id: 18, namaProduk: "Rok Mimi", penjual: "Ziri", tipe: "Baju", gender: 2, stock: 78, img: "produk/rok mimi.jpg", harga: 82000 },
+    { id: 18, namaProduk: "Rok Mimi", penjual: "Ziri", tipe: "Celana", gender: 2, stock: 78, img: "produk/rok mimi.jpg", harga: 82000 },
     { id: 19, namaProduk: "Celana Cut Slay", penjual: "CFW", tipe: "Celana", gender: 2, stock: 56, img: "produk/cutslay.jpg", harga: 30000 },
     { id: 20, namaProduk: "Celana Korporat65", penjual: "Eksekusi", tipe: "Celana", gender: 1, stock: 21, img: "produk/celanakorporat.jpg", harga: 38000 }
 ]
 
-console.log(daftarProduk)
 
-let cards = document.getElementById("cards")
-// console.log(cards)
-// cards.innerHTML = ''
+const checkboxPria = document.getElementById('cekBajuPria');
+const checkboxWanita = document.getElementById('cekBajuWanita');
+const searchButton = document.getElementById("searchButton");
+const searchText = document.getElementById("text-search");
 
-// let card = document.createElement("div")
-// card.classList.add("card")
-// // card.innerHTML = ''
-// console.log(card)
+ambilProduk(daftarProduk);
 
-// let card2 = document.createElement("div")
-// card2.classList.add("card")
-// // card.innerHTML = ''
-// console.log(card2)
+// ----------------- EVENT LISTENER ---------------------------------------
+checkboxPria.addEventListener('change', (event) => {
+    if (event.target.checked) {
+        console.log('checked')
+        if (checkboxWanita.checked) {
+            console.log('ke if')
+            filter('Baju Wanita Pria')
+        } else {
+            console.log('ke else')
+            filter('Baju Pria')
+        }
+    } else {
+        if (checkboxWanita.checked) {
+            filter('Baju Wanita')
+        } else {
+            ambilProduk(daftarProduk)
+        }
+    }
+});
 
-// // let card = document.createElement("div")
-// // card.classList.add("card")
+checkboxWanita.addEventListener('change', (event) => {
+    if (event.target.checked) {
+        if (checkboxPria.checked) {
+            filter('Baju Wanita Pria')
+        } else {
+            filter('Baju Wanita')
+        }
+    } else {
+        if (checkboxPria.checked) {
+            filter('Baju Pria')
+        } else {
+            ambilProduk(daftarProduk)
+        }
+    }
+});
 
-// let img = document.createElement('img')
-// img.src = daftarProduk[0].img
-// img.width = "220"
-// img.height = "150"
-// img.style.objectFit = "cover"
-// card.appendChild(img)
+searchButton.addEventListener("click", function () {
+    // console.log("Button clicked!");
+    if (searchText.value !== '') {
+        searchProduct(searchText.value)
+        searchText.value = ''
+    } else {
+        alert('Mau Cari Apaan Ngab ? Isi Dulu Kali');
+        ambilProduk(daftarProduk)
+    }
+});
+// ----------------- END OF EVENT LISTENER ---------------------------------
 
-// bikin nama produk
-// let nama = document.createElement('p')
-// nama.innerHTML = `<b>${daftarProduk.namaProduk}</b>`
-// card.appendChild(nama)
 
-// cards.appendChild(card)
-// cards.appendChild(card2)
+
+function clearData() {
+    const parentCards = document.getElementById('cards');
+    parentCards.innerHTML = '';
+}
 
 function ambilProduk(daftarProduk) {
-    for(produk of daftarProduk){
+    clearData()
+    for (produk of daftarProduk) {
         // membuat card
         let card = document.createElement("div")
         card.classList.add("card")
@@ -116,27 +146,27 @@ function ambilProduk(daftarProduk) {
             let moduloTiga = hargaString.length % 3;
             for (let i = 1; i <= hargaString.length; i++) {
                 if (i % 3 === moduloTiga & i !== hargaString.length) {
-                    hargaStringDenganTitik += hargaString[i-1] + '.'; 
+                    hargaStringDenganTitik += hargaString[i - 1] + '.';
                 } else {
-                    hargaStringDenganTitik += hargaString[i-1];
+                    hargaStringDenganTitik += hargaString[i - 1];
                 }
             }
             hargaString = hargaStringDenganTitik;
         }
-        
+
         harga.innerText = `Rp ${hargaString},00`
         card.appendChild(harga)
-        
+
         // memasukkan stok item
         let stok = document.createElement('span')
         stok.classList.add('stock') // 
         stok.innerText = `Stok: ${produk.stock}`
         card.appendChild(stok)
 
-        
+
         // memasukkan tombol 'langsung beli' dan 'tambah keranjang'
         let buttonCard = document.createElement('div')
-        
+        buttonCard.classList.add('button-card')
         let langsungBeli = document.createElement('button')
         langsungBeli.classList.add('button') // belom ada class button
         langsungBeli.setAttribute("id", `buttonLangsungBeli-${produk.id}`)
@@ -150,10 +180,116 @@ function ambilProduk(daftarProduk) {
         buttonCard.appendChild(tambahKeranjang)
 
         card.appendChild(buttonCard) // tombol ga ada jarak
-        
+
         // memasukkan card ke dalam cards
         cards.appendChild(card)
     }
 }
 
-ambilProduk(daftarProduk);
+function filter(data) {
+    clearData()
+    for (produk of daftarProduk) {
+        if (data == '' || !data) {
+            ambilProduk(daftarProduk);
+        }
+        else if (data == 'Baju Pria') {
+            if (produk.tipe === 'Baju' && produk.gender == 1) {
+                createCards()
+            }
+        }
+        else if (data == 'Baju Wanita') {
+            if (produk.tipe === 'Baju' && produk.gender == 2) {
+                createCards()
+            }
+        }
+        else if (data == 'Baju Wanita Pria') {
+            if (produk.tipe === 'Baju' && (produk.gender == 2 || produk.gender == 1 || produk.gender == 0)) {
+                createCards()
+            }
+        }
+    }
+}
+
+
+function searchProduct(searchTerm) {
+    const filteredProducts = daftarProduk.filter((produk) => {
+        return produk.namaProduk.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    clearData()
+    ambilProduk(filteredProducts)
+}
+
+
+function createCards() {
+    // membuat card
+    let card = document.createElement("div")
+    card.classList.add("card")
+    card.setAttribute("id", produk.id)
+
+    // memasukkan gambar produk
+    let img = document.createElement('img')
+    img.src = produk.img
+    img.width = "220"
+    img.height = "150"
+    // img.style.objectFit = "cover"
+    card.appendChild(img)
+
+    // memasukkan nama produk
+    let nama = document.createElement('span')
+    nama.classList.add('nama-barang')
+    nama.innerHTML = `<b>${produk.namaProduk}</b>` // !! bold mendingan masuk ke class ga sih?
+    card.appendChild(nama)
+
+    // memasukkan penjual
+    let penjual = document.createElement('span')
+    penjual.classList.add('penjual')
+    penjual.innerText = `Penjual: ${produk.penjual}`
+    card.appendChild(penjual)
+
+    // memasukkan harga produk
+    let harga = document.createElement('span')
+    harga.classList.add('harga')
+    let hargaString = String(produk.harga)
+    if (hargaString.length > 3) {
+        let hargaStringDenganTitik = '';
+        let moduloTiga = hargaString.length % 3;
+        for (let i = 1; i <= hargaString.length; i++) {
+            if (i % 3 === moduloTiga & i !== hargaString.length) {
+                hargaStringDenganTitik += hargaString[i - 1] + '.';
+            } else {
+                hargaStringDenganTitik += hargaString[i - 1];
+            }
+        }
+        hargaString = hargaStringDenganTitik;
+    }
+
+    harga.innerText = `Rp ${hargaString},00`
+    card.appendChild(harga)
+
+    // memasukkan stok item
+    let stok = document.createElement('span')
+    stok.classList.add('stock') // 
+    stok.innerText = `Stok: ${produk.stock}`
+    card.appendChild(stok)
+
+
+    // memasukkan tombol 'langsung beli' dan 'tambah keranjang'
+    let buttonCard = document.createElement('div')
+    buttonCard.classList.add('button-card')
+    let langsungBeli = document.createElement('button')
+    langsungBeli.classList.add('button') // belom ada class button
+    langsungBeli.setAttribute("id", `buttonLangsungBeli-${produk.id}`)
+    langsungBeli.innerText = 'Langsung Beli'
+    buttonCard.appendChild(langsungBeli)
+
+    let tambahKeranjang = document.createElement('button')
+    tambahKeranjang.classList.add('button')
+    tambahKeranjang.setAttribute("id", `buttonTambahKeranjang-${produk.id}`)
+    tambahKeranjang.innerText = 'Tambah Keranjang'
+    buttonCard.appendChild(tambahKeranjang)
+
+    card.appendChild(buttonCard) // tombol ga ada jarak
+
+    // memasukkan card ke dalam cards
+    cards.appendChild(card)
+}
